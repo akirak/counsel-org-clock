95d2d7e153627aab01d07584f914c993e366c1ad
Akira Komamura <akira.komamura@gmail.com>
2018-04-19 23:37:09 +0900

    Bump to 0.2

--- counsel-org-clock.el
+++ counsel-org-clock.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2018 by Akira Komamura
 
 ;; Author: Akira Komamura <akira.komamura@gmail.com>
-;; Version: 0.1.0
+;; Version: 0.2
 ;; Package-Requires: ((emacs "24.1") (ivy "0.10.0"))
 ;; URL: https://github.com/akirak/counsel-org-clock
 
